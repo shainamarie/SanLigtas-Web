e0aa8f7f4696cd53b767b0e0a30a63737ee13361
shainamarie <sweetmarie980@gmail.com>
2019-03-22 17:09:25 +0800

    UI needs to be responsive

--- routes.py
+++ routes.py
@@ -1,5 +1,6 @@
-from flask import Flask, render_template, flash, redirect, url_for, session, request, logging,send_from_directory
-import psycopg2
+from flask import Flask, render_template
+import requests
+import pytemperature
 
 
 app = Flask(__name__)
@@ -7,11 +8,27 @@ app.config.from_object(__name__)
 app.secret_key = 'secretkey'
 
 
+
 @app.route('/')
 def home():
-    return ('Hello world')
+	api_address = 'http://api.openweathermap.org/data/2.5/weather?appid=8f46c985e7b5f885798e9a5a68d9c036&q=Iligan'
+	json_data = requests.get(api_address).json()
+	city = json_data['name']
+	formatted_data = json_data['weather'][0]['description']
+	weather_icon = json_data['weather'][0]['icon']
+	temp = json_data['main']['temp']
+	final_temp = pytemperature.k2c(temp)
+	celcius = round(final_temp, 2)
+	print(city)
+	print(formatted_data)
+	print(weather_icon)
+	print(temp)
+	print(final_temp)
+	print(celcius)
+	return render_template('home.html', weather=formatted_data, weather_icon=weather_icon, celcius=celcius, city=city)
+	# return render_template('home.html')
 
 
 
 if __name__=='__main__':
-    app.run(debug=True)
+    app.run(debug=True)	
